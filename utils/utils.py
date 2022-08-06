9ed6866404e7bccc86b81a49db92fb5cb8510d86
Ryan Huang <zihaohuang@aliyun.com>
2022-08-06 20:13:45 +0800

    try

---
+++ utils/utils.py
@@ -0,0 +1,118 @@
+import cv2
+import os
+import math
+import random
+import numpy as np
+import numpy.random as npr
+# import torch
+# import torchvision.transforms as transforms
+import paddle.vision.transforms as transforms
+import paddle
+
+
+def init_seeds(seed=0):
+    random.seed(seed)
+    np.random.seed(seed)
+    paddle.seed(seed)
+
+    # Remove randomness (may be slower on Tesla GPUs) 
+    # https://pytorch.org/docs/stable/notes/randomness.html
+    if seed == 0:
+        # cuDNN对于同一操作有几种算法，一些算法结果是非确定性的，如卷积算法。
+        # 该flag用于调试。它表示是否选择cuDNN中的确定性函数。
+        flags = {
+            "FLAGS_cudnn_deterministic": True,
+            }
+        paddle.set_flags(flags)
+
+
+def hyp_parse(hyp_path):
+    
+    """
+    将 hyp.py 文件解析
+    返回一个参数字典
+    """
+    hyp = {}
+    keys = [] 
+    with open(hyp_path,'r') as f:
+        for line in f:
+            if line.startswith('#') or len(line.strip())==0 : continue
+            v = line.strip().split(':')
+            try:
+                hyp[v[0]] = float(v[1].strip().split(' ')[0])
+            except:
+                hyp[v[0]] = eval(v[1].strip().split(' ')[0])
+            keys.append(v[0])
+        f.close()
+    return hyp
+
+
+
+
+def rescale(im, target_size, max_size, keep_ratio, multiple=32):
+    im_shape = im.shape
+    im_size_min = np.min(im_shape[0:2])
+    im_size_max = np.max(im_shape[0:2])
+    if keep_ratio:
+        # method1
+        im_scale = float(target_size) / float(im_size_min)  
+        if np.round(im_scale * im_size_max) > max_size:     
+            im_scale = float(max_size) / float(im_size_max)
+        im_scale_x = np.floor(im.shape[1] * im_scale / multiple) * multiple / im.shape[1]
+        im_scale_y = np.floor(im.shape[0] * im_scale / multiple) * multiple / im.shape[0]
+        im = cv2.resize(im, None, None, fx=im_scale_x, fy=im_scale_y, interpolation=cv2.INTER_LINEAR)
+        im_scale = np.array([im_scale_x, im_scale_y, im_scale_x, im_scale_y])
+        # method2
+        # im_scale = float(target_size) / float(im_size_max)
+        # im = cv2.resize(im, None, None, fx=im_scale, fy=im_scale, interpolation=cv2.INTER_LINEAR)
+        # im_scale = np.array([im_scale, im_scale, im_scale, im_scale])
+
+    else:
+        target_size = int(np.floor(float(target_size) / multiple) * multiple)
+        im_scale_x = float(target_size) / float(im_shape[1])
+        im_scale_y = float(target_size) / float(im_shape[0])
+        im = cv2.resize(im, (target_size, target_size), interpolation=cv2.INTER_LINEAR)
+        im_scale = np.array([im_scale_x, im_scale_y, im_scale_x, im_scale_y])
+    return im, im_scale
+
+
+class Rescale(object):
+    def __init__(self, target_size=600, max_size=2000, keep_ratio=True):
+        self._target_size = target_size
+        self._max_size = max_size
+        self._keep_ratio = keep_ratio
+
+    def __call__(self, im):
+        if isinstance(self._target_size, list):
+            random_scale_inds = npr.randint(0, high=len(self._target_size))
+            target_size = self._target_size[random_scale_inds]
+        else:
+            target_size = self._target_size
+        im, im_scales = rescale(im, target_size, self._max_size, self._keep_ratio)
+        return im, im_scales
+
+
+class Normailize(object):
+    def __init__(self):
+        # RGB: https://github.com/pytorch/vision/issues/223
+        self._transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))  # 均值和方差
+        ])
+
+    def __call__(self, im):
+        im = self._transform(im)
+        return im
+
+
+class Reshape(object):
+    def __init__(self, unsqueeze=True):
+        self._unsqueeze = unsqueeze
+        return
+
+    def __call__(self, ims):
+        if not paddle.is_tensor(ims):
+            ims = paddle.to_tensor(ims.transpose((2, 0, 1)))
+        if self._unsqueeze:
+            ims = ims.unsqueeze(0)
+        return ims
